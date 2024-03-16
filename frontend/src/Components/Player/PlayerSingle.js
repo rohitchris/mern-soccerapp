318a019a35a8efd62b2fb487dd76d0f7f137be72
Rohit D'Souza <rohit@tomorrownetworks.com>
2024-03-16 13:59:58 +0530

    frontend files

---
+++ frontend/src/Components/Player/PlayerSingle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PlayerSingle = (props) => {
+  return (
+    <div className='row'>
+      <div className='col s12 m7'>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='./soccer.jpeg' alt='profile' />
+            <span className='card-title'>
+              {props.player.firstName} {props.player.lastName}
+            </span>
+          </div>
+          <div className='card-content'>
+            <p>Phone: {props.player.phone}</p>
+            <p>Email: {props.player.email}</p>
+            <p>Strength: {props.player.strength}</p>
+          </div>
+          <div className='card-action'>Team: {props.player.team}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerSingle;
